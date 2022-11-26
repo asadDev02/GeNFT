4ee9879961492683be4f4d6326edb99800c075f2
Muhammad-Asad-Mughal <asadmughal1785@gmail.com>
2022-11-26 19:42:32 +0500

    commit

--- app/src/main/java/com/example/mobileapp/SplashScreen.java
+++ app/src/main/java/com/example/mobileapp/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.mobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+        new Handler().postDelayed(() -> {
+            startActivity(intent);
+            finish();
+        },3000);
     }
 }
