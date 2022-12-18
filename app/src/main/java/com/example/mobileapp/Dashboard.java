e624c971069e59ca6c9869a8d0f31b282498fcba
Muhammad-Asad-Mughal <asadmughal1785@gmail.com>
2022-12-18 15:36:52 +0500

    commit

--- app/src/main/java/com/example/mobileapp/Dashboard.java
+++ app/src/main/java/com/example/mobileapp/Dashboard.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class Dashboard extends AppCompatActivity {
 
     TextView  email1;
     TextView pass1;
+    TextView dice;
+    int min = 1;
+    int max = 6;
+    Random random = new Random();
+    int number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,16 @@ public class Dashboard extends AppCompatActivity {
         pass1 = findViewById(R.id.password1);
         email1.setText(email);
         pass1.setText(pass);
+
+    }
+
+    public void roll(View view) {
+        dice = findViewById(R.id.dice);
+        number = random.nextInt(max + min)+ min;
+        dice.setText("Rolling.....");
+        new Handler().postDelayed(() -> {
+            dice.setText(String.valueOf(number));
+        },800);
 
     }
 }
